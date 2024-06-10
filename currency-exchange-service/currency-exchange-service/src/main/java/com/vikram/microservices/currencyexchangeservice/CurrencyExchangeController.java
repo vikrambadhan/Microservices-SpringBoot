9cca42874540b9946782db83e193b6b21ef63678
vikram.badhan <vikram.badhan@gmail.com>
2024-06-10 15:20:11 -0500

    adding project files

---
+++ currency-exchange-service/currency-exchange-service/src/main/java/com/vikram/microservices/currencyexchangeservice/CurrencyExchangeController.java
@@ -0,0 +1,37 @@
+package com.vikram.microservices.currencyexchangeservice;
+
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.boot.autoconfigure.couchbase.CouchbaseProperties;
+import org.springframework.core.env.Environment;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.PathVariable;
+import org.springframework.web.bind.annotation.RestController;
+
+import java.math.BigDecimal;
+
+@RestController
+public class CurrencyExchangeController {
+
+    @Autowired
+    private CurrencyExchangeRepository repository;
+
+    @Autowired
+    private Environment environment;
+
+    @GetMapping("/currency-exchange/from/{from}/to/{to}")
+    public CurrencyExchange currencyExchangeValue(
+            @PathVariable String from,
+            @PathVariable String to) {
+//        CurrencyExchange currencyExchange = new CurrencyExchange(1000L, from, to,
+//                BigDecimal.valueOf(50));
+        CurrencyExchange currencyExchange =repository.findByFromAndTo(from, to);
+
+        if(currencyExchange == null) {
+            throw new RuntimeException("Enable to find data for"+ from +"to" +to);
+        }
+        String port = environment.getProperty("local.server.port");
+        currencyExchange.setEnvironment(port);
+
+        return currencyExchange;
+    }
+}
